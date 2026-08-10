0098268bf502eab3d326802deba1e4ac881c1548
Muhammad Ridho Alfarod <muhridhoalfarod@gmail.com>
2026-08-10 11:37:23 +0800

    refactor: add reusable brand logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,3 @@
+export default function Logo() {
+  return <a className="nav-logo" href="#beranda" aria-label="Nusantara Digital beranda"><span className="logo-icon">N</span><span>Nusantara <span className="logo-text-highlight">Digital</span></span></a>
+}
