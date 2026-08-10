ed99c2f10cb561017ab2e0734ebe94d6d27084db
Muhammad Ridho Alfarod <muhridhoalfarod@gmail.com>
2026-08-10 11:37:23 +0800

    refactor: add site footer component

---
+++ src/components/SiteFooter.jsx
@@ -0,0 +1,3 @@
+export default function SiteFooter() {
+  return <footer className="footer"><div className="container"><div className="footer-bottom"><p>© 2026 PT Nusantara Digital. Seluruh hak dilindungi.</p><p>Dibuat dengan semangat untuk Indonesia.</p></div></div></footer>
+}
